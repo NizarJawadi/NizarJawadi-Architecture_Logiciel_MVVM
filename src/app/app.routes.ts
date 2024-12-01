48f3702c17127ba06381f61f0442c4c6b623e7b9
Jawadi Nizar <106398838+NizarJawadi@users.noreply.github.com>
2024-12-01 19:11:53 +0100

    initiation de projet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { StockComponent } from './stock/stock.component';
+import { ProduitComponent } from './produit/produit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"header" , component: HeaderComponent},
+    {path:"produit" , component: ProduitComponent},
+    {path:"stock" , component: StockComponent},
+];
